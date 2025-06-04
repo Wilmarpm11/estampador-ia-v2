497352ae19519150ad23d6b274f19ab630f07d83
Wilmarpm11 <Wilmarpm1311@gmail.com>
2025-06-03 21:32:48 -0300

    geradorPrompt.js

---
+++ utils/geradorPrompt.js
@@ -0,0 +1,3 @@
+export function gerarPrompt(estilo, cores, fundo) {
+  return `Crie uma estampa detalhada e harmoniosa com o estilo ${estilo}, usando as cores ${cores}, em um fundo ${fundo}. A estampa deve ser vibrante, com detalhes nítidos e adequada para impressão em tecido.`;
+}
