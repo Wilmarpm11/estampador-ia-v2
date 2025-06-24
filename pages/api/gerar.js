e92f66f7e115ab9548b72a84adeb26b9ec65fe30
Wilmarpm11 <Wilmarpm1311@gmail.com>
2025-06-24 18:01:38 -0300

    Update gerar.js

--- pages/api/gerar.js
+++ pages/api/gerar.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
     const response = await openai.images.generate({
       prompt,
       n: 1,
-      size: "512x512", // Retornando a tamanho menor para evitar limite
+      size: "512x512",
       response_format: "b64_json",
     });
 
